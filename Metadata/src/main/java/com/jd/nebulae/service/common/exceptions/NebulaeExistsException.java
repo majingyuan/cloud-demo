8a46b701d0592808eceaeeaa39dc4bcf62419d41
majingyuan <majingyuan@jd.com>
2020-04-03 17:06:24 +0800

    delete

--- Metadata/src/main/java/com/jd/nebulae/service/common/exceptions/NebulaeExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.jd.nebulae.service.common.exceptions;
-
-@SuppressWarnings("AlibabaClassMustHaveAuthor")
-public class NebulaeExistsException extends RuntimeException {
-
-    public NebulaeExistsException(String message) {
-        super(String.format("%s already exists", message));
-    }
-}
--- Metadata/src/main/java/com/jd/nebulae/service/common/exceptions/NebulaeNotExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.jd.nebulae.service.common.exceptions;
-
-@SuppressWarnings("AlibabaClassMustHaveAuthor")
-public class NebulaeNotExistsException extends RuntimeException {
-
-    public NebulaeNotExistsException(String message) {
-        super(String.format("%s not exists", message));
-    }
-}
